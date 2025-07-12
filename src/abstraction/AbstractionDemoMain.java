c83081f9ae92a40f1655c6264aaaf8431e5b7525
survasebharat <survasebharat7@gmail.com>
2025-07-12 16:22:22 +0530

    abstraction exmaple on Vehicle

---
+++ src/abstraction/AbstractionDemoMain.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+public class AbstractionDemoMain {
+
+	public static void main(String[] args) {
+		
+		Vehicle v=new Car();
+		String engin = v.engin();
+		System.out.println(engin);
+
+	}
+
+}
---
+++ src/abstraction/Car.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public class Car implements Vehicle {
+
+	@Override
+	public String engin() {
+		
+		return "Car has Diesel Engin";
+	}
+
+}
